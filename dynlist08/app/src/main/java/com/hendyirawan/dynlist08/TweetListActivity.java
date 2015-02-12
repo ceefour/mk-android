781da8af24cc8d262a9b7db8f7e3c783b3633d0f
Hendy Irawan <ceefour666@gmail.com>
2015-02-12 18:18:13 +0700

    module 8

---
+++ dynlist08/app/src/main/java/com/hendyirawan/dynlist08/TweetListActivity.java
@@ -0,0 +1,71 @@
+package com.hendyirawan.dynlist08;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.hendyirawan.dynlist08.models.Tweet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TweetListActivity extends ListActivity {
+
+    protected final String[] prefixes = new String[] { "Car", "Bird", "Elephant", "Phone", "Building" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_list);
+
+        final List<Tweet> tweets = new ArrayList<>();
+        final Random rand = new Random();
+        for (int i = 0; i < 100; i++) {
+            final String name = prefixes[rand.nextInt(prefixes.length)] + ' ' + (rand.nextInt(10) + 1);
+            final Tweet tweet = new Tweet((long) i, name, "Some random body for " + name);
+            tweets.add(tweet);
+        }
+//        final ArrayAdapter<String> tweetsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+//                names.toArray(new String[]{}));
+        final TweetAdapter tweetsAdapter = new TweetAdapter(this, tweets.toArray(new Tweet[] {}));
+
+        final ListView lvTweet = getListView();
+        lvTweet.setAdapter(tweetsAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tweet_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+//        final TextView itemBody = (TextView) v.findViewById(R.id.tv_body);
+//        itemBody.setText("I was clicked!!");
+        final Intent intent = new Intent(this, TweetDetailActivity.class);
+        startActivity(intent);
+    }
+}
