1a7ce5ae4761a42fb44c09c50a6f1d53ca63dca9
Hendy Irawan <ceefour666@gmail.com>
2015-02-12 20:29:31 +0700

    module 10 done

---
+++ asynctask10/app/src/main/java/com/hendyirawan/asynctask10/AsyncFetchTweets.java
@@ -0,0 +1,64 @@
+package com.hendyirawan.asynctask10;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.hendyirawan.asynctask10.models.Tweet;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by ceefour on 2/12/15.
+ */
+public class AsyncFetchTweets extends AsyncTask<Void, Void, Void> {
+
+    protected final String[] prefixes = new String[] { "Car", "Bird", "Elephant", "Phone", "Building" };
+
+    private final TweetListActivity activity;
+    private final List<Tweet> tweets;
+
+    public AsyncFetchTweets(TweetListActivity activity, List<Tweet> tweets) {
+        this.activity = activity;
+        this.tweets = tweets;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        final Random rand = new Random();
+        for (int i = 0; i < 5; i++) {
+            final String name = prefixes[rand.nextInt(prefixes.length)] + ' ' + (rand.nextInt(10) + 1);
+            final Tweet tweet = new Tweet((long) i, name, "Some random body for " + name);
+//            tweetsAdapter.insert(tweet, 0);
+            tweets.add(0, tweet);
+        }
+        Log.i(AsyncFetchTweets.class.getSimpleName(), "Prepended random tweets to list, now: " + tweets.size());
+
+//        final ArrayAdapter<String> tweetsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+//                names.toArray(new String[]{}));
+        new AsyncWriteTweets(activity, tweets).execute();
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        Log.i(AsyncFetchTweets.class.getSimpleName(), "Updated tweets, now: " + tweets.size());
+        activity.renderTweets();
+    }
+}
---
+++ asynctask10/app/src/main/java/com/hendyirawan/asynctask10/MainActivity.java
@@ -0,0 +1,83 @@
+package com.hendyirawan.asynctask10;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String USERNAME_KEY = "username";
+    public static final String PASSWORD_KEY = "password";
+    private Button _loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Log.i(MainActivity.class.getName(), "Created");
+        final EditText usernameFld = (EditText) findViewById(R.id.usernameFld);
+        final EditText passwordFld = (EditText) findViewById(R.id.passwordFld);
+        final SharedPreferences prefs = getSharedPreferences("codelearn_twitter", MODE_PRIVATE);
+        final String username = prefs.getString(USERNAME_KEY, null);
+        final String password = prefs.getString(PASSWORD_KEY, null);
+        if (username != null && password != null) {
+            // already logged in
+            usernameFld.setText(username);
+            passwordFld.setText(password);
+            final Intent intent = new Intent(MainActivity.this, TweetListActivity.class);
+            startActivity(intent);
+        }
+
+        _loginBtn = (Button) findViewById(R.id.btn_login);
+        _loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(MainActivity.class.getName(), "Clicked");
+                //_loginBtn.setText("I am clicked!");
+
+                final String username = usernameFld.getText().toString();
+                final String password = passwordFld.getText().toString();
+                Log.d(MainActivity.class.getName(), "Username: " + username);
+
+                final SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(USERNAME_KEY, username);
+                editor.putString(PASSWORD_KEY, password);
+                editor.commit();
+
+                final Intent intent = new Intent(MainActivity.this, TweetListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
