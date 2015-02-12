6ae2f7a7531c0f9f2f90d72b9ba3ebfb7a2b1109
Hendy Irawan <ceefour666@gmail.com>
2015-02-12 17:15:35 +0700

    module 6 done

---
+++ tweetlist06/app/src/main/java/com/hendyirawan/tweetlist06/MainActivity.java
@@ -0,0 +1,56 @@
+package com.hendyirawan.tweetlist06;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity {
+
+    private Button _loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Log.i(MainActivity.class.getName(), "Created");
+        _loginBtn = (Button) findViewById(R.id.btn_login);
+        _loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(MainActivity.class.getName(), "Clicked");
+                //_loginBtn.setText("I am clicked!");
+                final Intent intent = new Intent(MainActivity.this, TweetListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
